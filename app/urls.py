97262db7fad381de3d0b33ae644b187d485631c4
Isaí Jeremías <isa.farias@duocuc.cl>
2023-07-06 19:14:35 -0400

    Proyrcto con url y static

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views
+from app.views import index
+from app.views import iniciosesion
+from app.views import carrito
+from app.views import contacto
+from app.views import Login
+from app.views import RegistrarUsuario
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    path('app/iniciosesion.html', views.iniciosesion, name='iniciosesion'),
+    path('app/Carrito.html', views.carrito, name='Carrito'),
+    path('app/Contact.html', views.contacto, name='Contact'),
+    path('app/Login.html', views.Login, name='Login'),
+    path('app/RegistrarUsuario.html', views.RegistrarUsuario, name='RegistrarUsuario'),
+] 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Noticia
 
 def index(request):
+
     return render(request, 'app/index.html', {})
-# Create your views here.
+
+def iniciosesion(request):
+    return render(request, 'app/iniciosesion.html', {})
+
+def carrito(request):
+    return render(request, 'app/Carrito.html', {})
+
+def contacto(request):
+    return render(request, 'app/Contact.html', {})
+
+def Login(request):
+    return render(request, 'app/Login.html', {})
+
+def RegistrarUsuario(request):
+    return render(request, 'app/RegistrarUsuario.html', {})
+
+#def noticia_list(request):
+    #noticias = Noticia.objects
+    #return render(request, 'app/index.html', {'Noticia': noticias})
